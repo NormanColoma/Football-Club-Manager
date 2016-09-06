a33626538b8171e0f168d7283fd4e4b3f64647c2
Norman Coloma <ua.norman@gmail.com>
2016-09-06 12:08:01 +0200

    Database was configured
    -Dependencies for JPA and psql driver were added
    -Database connenction info was included in conf/application.conf

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,23 +1,30 @@
 package controllers;
 
 import play.mvc.*;
+import play.libs.Json.*;
+import play.libs.Json;
+import javax.inject.Inject;
 
-import views.html.*;
-
+import static play.libs.Json.toJson;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.db.*;
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    private Database db;
+
+    @Inject
+    public HomeController(Database db) {
+        this.db = db;
+    }
+
     public Result index() {
-        return ok(index.render());
+        JsonNode json = Json.parse("{\"firstName\":\"Foo\", \"lastName\":\"Bar\", \"age\":13}");
+        return ok(json);
     }
 
 }
--- build.sbt
+++ build.sbt
@@ -6,8 +6,14 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 scalaVersion := "2.11.7"
 
+#Need to add JPA driver and PSQL (in this case) driver
 libraryDependencies ++= Seq(
   javaJdbc,
   cache,
-  javaWs
+  javaWs,
+  javaJpa,
+  "org.hibernate" % "hibernate-entitymanager" % "5.1.0.Final",
+  "org.postgresql" % "postgresql" % "9.4-1206-jdbc42"
 )
+
+PlayKeys.externalizeResources := false
